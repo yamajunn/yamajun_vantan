6f540bc0b7e84498c8ffb3884cfd972b4a349dcf
yamajunn <117433834+yamajunn@users.noreply.github.com>
2023-03-28 15:05:20 +0900

    Add files via upload

--- minecraft.py
+++ minecraft.py
@@ -1,6 +1,8 @@
 import os
 import cv2
 import numpy as np
+from PIL import Image
+import copy
 
 path = "./images1"
 files = os.listdir(path)
@@ -24,25 +26,28 @@ for file in files:
     # 画像の縦横
     h, w = img.shape[:2]
 
+    img = cv2.resize(img, (int(w*4), int(h*4)))
+
+    h, w = h*4, w*4
     image_list = []
 
-    # 色の変更
     for i in range(h):
         for j in range(w):
             b, g, r, a = img[i, j]
+            if a > 125:
+                img[i, j] = 0, 0, 0, 255
+            else:
+                img[i, j] = 0, 0, 0, 0
+            b, g, r, a = img[i, j]
             image_list.append(a)
-            if a != 0:
-                img[i, j] = change_color
-            # else:
-            #     img[i, j] = change_color2
     
     image_t_f_list = []
     hight_list = []
     hight_list2 = []
     for i in range(h):
-        if i % 16 == 0:
+        if i % w == 0:
             hight_list.append(i)
-            hight_list2.append(i+15)
+            hight_list2.append(i+w-1)
 
     for i in range(h*w):
         if (i != 0 and image_list[i] == 255 and image_list[i-1] == 0) or (i != h*w-1 and image_list[i] == 255 and image_list[i+1] == 0):
@@ -54,9 +59,9 @@ for file in files:
             image_t_f_list[i] = True
         if i % w == w-1 and image_list[i] != 0:
             image_t_f_list[i] = True
-        if i // 16 in hight_list and image_list[i] != 0:
+        if i // w in hight_list and image_list[i] != 0:
             image_t_f_list[i] = True
-        if i // 16 in hight_list2 and image_list[i] != 0:
+        if i // w in hight_list2 and image_list[i] != 0:
             image_t_f_list[i] = True
 
     for i in range(h):
